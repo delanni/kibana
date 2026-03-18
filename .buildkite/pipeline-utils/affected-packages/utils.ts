c397186ce08575bab21b98af0d9590c5fe2c37f7
Alex Szabo <alex.szabo@elastic.co>
2026-03-18 16:54:43 +0100

    disable the critical file check to see the filtering in action

--- .buildkite/pipeline-utils/affected-packages/utils.ts
+++ .buildkite/pipeline-utils/affected-packages/utils.ts
@@ -24,7 +24,9 @@ export function filterIgnoredFiles(files: string[], patterns: string[]): string[
  * Returns true when any pattern matches any file in the list
  */
 export function touchedCriticalFiles(files: string[], criticalFiles: string[]): boolean {
-  return files.some((file) =>
-    criticalFiles.some((criticalFile) => minimatch(file, criticalFile, { dot: true }))
+  return (
+    files.some((file) =>
+      criticalFiles.some((criticalFile) => minimatch(file, criticalFile, { dot: true }))
+    ) && false // temporary disable
   );
 }
