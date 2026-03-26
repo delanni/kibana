f176b0d5a17f1f214bc59d4d87d80d9309556891
Alex Szabo <alex.szabo@elastic.co>
2026-03-26 13:00:00 +0100

    try erasing prior cache before archiving

--- .buildkite/scripts/steps/store_cache.sh
+++ .buildkite/scripts/steps/store_cache.sh
@@ -4,13 +4,22 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
+du -h .moon/cache
+
+echo "--- Clear moon cache"
+rm -rf .moon/cache
+
 export MOON_CACHE=write
 .buildkite/scripts/bootstrap.sh
+
 echo "--- Archive moon cache"
 if [[ ! -d .moon/cache ]]; then
   echo "No moon cache directory found, skipping archive"
   exit 0
 else
+  du -h .moon/cache
+
+  echo "--- Archive moon cache"
   tar -czf ~/moon-cache.tar.gz .moon/cache || echo "Failed to archive moon cache"
   cd ~/
   buildkite-agent artifact upload moon-cache.tar.gz || echo "Failed to upload moon cache"
