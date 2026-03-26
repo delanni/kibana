ed987dc313c1b379791f3cf873a385a6e1d05dd9
Alex Szabo <alex.szabo@elastic.co>
2026-03-26 22:35:28 +0100

    remove investigative logging

--- .buildkite/scripts/steps/store_cache.sh
+++ .buildkite/scripts/steps/store_cache.sh
@@ -4,13 +4,6 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
-if [[ -d .moon/cache ]]; then
-  du -h .moon/cache
-fi
-
-echo "--- Clear moon cache"
-rm -rf .moon/cache
-
 export MOON_CACHE=write
 .buildkite/scripts/bootstrap.sh
 
@@ -19,12 +12,6 @@ if [[ ! -d .moon/cache ]]; then
   echo "No moon cache directory found, skipping archive"
   exit 0
 else
-  du -h .moon/cache
-  du -h .moon/cache/outputs
-
-  node .buildkite/scripts/steps/processMoonCache.cjs .moon/cache || echo "Failed to process moon cache"
-
-  echo "--- Archive moon cache"
   tar -czf ~/moon-cache.tar.gz .moon/cache || echo "Failed to archive moon cache"
   cd ~/
   buildkite-agent artifact upload moon-cache.tar.gz || echo "Failed to upload moon cache"
